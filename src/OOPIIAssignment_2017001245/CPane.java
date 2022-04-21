0375782e1b604a3f2ce4460a6ece6818355960c5
Matthew Cole <m.b.cole300@gmail.com>
2022-04-21 11:03:14 -0500

    Created CPane Class

---
+++ src/OOPIIAssignment_2017001245/CPane.java
@@ -0,0 +1,24 @@
+package OOPIIAssignment_2017001245;
+
+import java.awt.*;
+import java.util.ArrayList;
+import  java.util.List;
+
+
+public  class  CPane {
+
+    static class ContentP{
+
+        private static List<Container> panes = new ArrayList<Container>();
+
+        public static Container getLastPane(){
+
+            return panes.get(panes.size() - 1);
+        }
+
+        public static void addPane(Container pane){
+
+            panes.add(pane);
+        }
+    }
+}
